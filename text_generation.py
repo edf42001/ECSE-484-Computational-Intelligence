72e51c0f90b635bfb4b47f08492a1bd807d632dc
Ethan Frank <edf32@case.edu>
2022-04-14 18:19:00 -0400

    Remove old non-stateful code

--- text_generation.py
+++ text_generation.py
@@ -1,65 +1,44 @@
 from tensorflow import keras
 from tensorflow.keras import layers
-from keras.models import Model
 
 import numpy as np
-import random
-import io
 import os
 from datetime import datetime
 import matplotlib.pyplot as plt
 
 from custom_text_gen_callback import CustomTextGenCallback
+from custom_reset_state_callback import CustomResetStateCallback
+from prepare_training_data_from_file import stateful_training_data_from_file
 
 # Tutorial here:
 # https://keras.io/examples/generative/lstm_character_level_text_generation/
 
 base_path = "./"
-data_path = base_path + "input/courses/csds.txt"
+# data_path = base_path + "input/courses/csds.txt"
+data_path = base_path + "input/4.1_python.txt"
 
-# Read the file, all lowercase text to start with
-with io.open(data_path, encoding="utf-8") as f:
-    text = f.read().lower()
-print("Text length:", len(text))
 
-# Extract the number of different characters in the text
-# Create lookup tables for character/index
-chars = sorted(list(set(text)))
-char_indices = dict((c, i) for i, c in enumerate(chars))
-indices_char = dict((i, c) for i, c in enumerate(chars))
-num_chars = len(chars)
-print("Number of different chars:", num_chars)
+batch_size = 128
+sequence_len = 50
 
-# Split the text into chunks. The chunks overlap. This helps training?
-maxlen = 40
-step = 3
-sentences = []
-next_chars = []  # The next character given the previous maxlen context characters
-for i in range(0, len(text) - maxlen, step):
-    sentences.append(text[i : i + maxlen])
-    next_chars.append(text[i + maxlen])
+epochs = 60
+epoch_save_freq = 5
 
-print("Number of sequences:", len(sentences))
-# print(sentences)
-# print(next_chars)
+gen_text_freq = 20
+gen_text_length = 100
 
-# One hot encoded input sequences and output character
-x = np.zeros((len(sentences), maxlen, len(chars)), dtype=bool)
-y = np.zeros((len(sentences), len(chars)), dtype=bool)
-
-# Create the one hot encodings
-for i, sentence in enumerate(sentences):
-    for t, char in enumerate(sentence):
-        x[i, t, char_indices[char]] = 1
-    y[i, char_indices[next_chars[i]]] = 1
+ret = stateful_training_data_from_file(data_path, batch_size, sequence_len, validation_split=0.05)
+X_train, Y_train, X_validate, Y_validate, char_indices, indices_char, text, chars = ret
+num_chars = len(char_indices)
+print("Chars:", chars)
 
 # Model: a single LSTM layer TODO? Add dropout
 model = keras.Sequential(
     [
-        keras.Input(shape=(maxlen, num_chars)),
-        layers.LSTM(64, return_sequences=True),
+        keras.Input(batch_input_shape=(batch_size, sequence_len, num_chars)),  # shape=(maxlen, num_chars),
+        layers.LSTM(64, return_sequences=True, stateful=True),
         layers.Dropout(0.0),
-        layers.LSTM(64),
+        layers.LSTM(64, return_sequences=True, stateful=True),
         layers.Dropout(0.0),
         layers.Dense(num_chars, activation="softmax")
     ]
@@ -70,38 +49,40 @@ if not os.path.isdir(base_path + "model_checkpoints"):
     print("model_checkpoints directory not found, creating")
     os.mkdir(base_path + "model_checkpoints")
 
-
 # Create a model checkpoint that will save all models to a folder with the current time,
 # and a file name with epoch, loss, and validation loss in it
 current_time = datetime.now().strftime("%Y%m%d_%H%M%S")
 os.mkdir(base_path + "model_checkpoints" + "/" + current_time)
 checkpoint_filepath = os.path.join(base_path, "model_checkpoints", current_time, "{epoch:02d}-{loss:.2f}-{val_loss:.2f}.hdf5")
 
+# Freq is measured in batches
 model_checkpoint_cb = keras.callbacks.ModelCheckpoint(
     filepath=checkpoint_filepath,
     save_weights_only=False,
     save_best_only=False,
-    verbose=True)
+    verbose=True,
+    period=epoch_save_freq  # period is deprecated but this is the only way to get "val_loss" into the format string
+)
 
 # Create a custom callback to generate sample text every couple of epochs
-custom_text_gen_cb = CustomTextGenCallback(4, 50, maxlen, text, chars, char_indices, indices_char)
+custom_text_gen_cb = CustomTextGenCallback(gen_text_freq, gen_text_length, text, char_indices, indices_char)
 
-# TODO: use adam?
+# Reset the model every epoch, so the statefulness isn't messed up
+custom_reset_state_cb = CustomResetStateCallback()
+
+# TODO: Custom step down learning rate scheduler:
+# https://keras.io/api/callbacks/learning_rate_scheduler/
+
 optimizer = keras.optimizers.RMSprop(learning_rate=0.01, clipnorm=5)
 model.compile(loss="categorical_crossentropy", optimizer=optimizer)
 
 # Print a summary of our model
 model.summary()
 
-epochs = 7
-batch_size = 128
-
 # If the custom_text_gen_cb comes after the model_checkpoint_cb, then the history dictionary becomes empty
-history = model.fit(x, y, batch_size=batch_size, epochs=epochs,
-                    callbacks=[model_checkpoint_cb],
-                    validation_split=0.05, shuffle=False)
-
-print(history.history)
+history = model.fit(X_train, Y_train, batch_size=batch_size, epochs=epochs,
+                    callbacks=[model_checkpoint_cb, custom_reset_state_cb, custom_text_gen_cb],
+                    shuffle=False, validation_data=(X_validate, Y_validate))
 
 loss = history.history['loss']
 val_loss = history.history['val_loss']
