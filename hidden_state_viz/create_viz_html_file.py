21c45102998b92c5cb9758960e73dcc4694e334d
Ethan Frank <edf32@case.edu>
2022-04-15 10:25:32 -0400

    Improve hidden state viz for 3 layer model

--- hidden_state_viz/create_viz_html_file.py
+++ hidden_state_viz/create_viz_html_file.py
@@ -2,6 +2,7 @@ import numpy as np
 
 
 def rgb_to_hex(rgb):
+    # Converts a tuple to a hex string, for use by css
     return '%02x%02x%02x' % rgb
 
 
@@ -12,34 +13,32 @@ viz_file = "hidden_state.html"
 with open(text_file, 'r') as f:
     generated_text = f.read()
 
-
+# Read the activations, squash between -1 and 1 and multiply by 0.6 so the colors aren't too dark
 data = np.loadtxt(data_file, delimiter=",")
-data = np.tanh(data)
+data = 0.6 * np.tanh(data)
 
 print("Text: " + generated_text)
 length = len(generated_text)
 
-# Notes: Neuron 309 becomes inactive on "units".
+# Monospaced font header
+html_string = "<head><style type = \"text/css\">div {font-family: monospace}</style></head>\n"
 
-html_string = ""
-# TODO: investigate node 1300's output is out of range
-# 1311 is the parenthase neuron
-for node_index in range(data.shape[1]):
+for node_index in range(data.shape[1] - 256, data.shape[1] - 128):
     activations_colored_text = ""
     for i in range(length):
-        node_activation = data[i, node_index]
+        # When we record the data, we feed a character in, and record the output char and node activations
+        # But the node activations indicate the current state, which is dependent on the char we feed in
+        # Imagine we feed in a ", and the network is now in the quote state, the quote should be colored
+        # Anyway, that's why there is a plus 1 here
+        node_activation = data[min(i+1, length-1), node_index]
 
+        # Convert node activations less than one to blue, above to red
         if node_activation < 0:
             rgb = (int(255 + 255 * node_activation), int(255 + 255 * node_activation), int(255))
         else:
             rgb = (int(255), int(255 - 255 * node_activation), int(255 - 255 * node_activation))
 
-        # Debug node_activations->color mapping
-        # print(rgb_to_hex(rgb), node_activation)
-
         color = "#" + rgb_to_hex(rgb)
-        # if len(color) > 7:
-        #     print(rgb, color, node_activation)
 
         # Hardcoded cases to deal with annoying html things such as spaces at start of line not showing up
         if generated_text[i] == "\n":
@@ -51,7 +50,8 @@ for node_index in range(data.shape[1]):
 
         activations_colored_text += char_text
 
-    node_div_text = "<div id={}>{}</div>\n".format(node_index, activations_colored_text)
+    # Wrap each set of colored text in a div with a horizontal line in between
+    node_div_text = "<div id={}>{}</div><hr>\n".format(node_index, activations_colored_text)
     html_string += node_div_text
 
 # Save to the html file
--- hidden_state_viz/generate_text_and_save_hidden_neurons.py
+++ hidden_state_viz/generate_text_and_save_hidden_neurons.py
@@ -1,45 +1,49 @@
 from tensorflow import keras
-from tensorflow.keras import layers
 from keras.layers import LSTM, Input, Dense
 from keras.models import Model
 
 import numpy as np
 import random
-import io
+import time
 
 from custom_text_gen_callback import sample
 from prepare_training_data_from_file import stateful_training_data_from_file
 
 base_path = "../"
-data_path = base_path + "input/courses/csds.txt"
+# data_path = base_path + "input/courses/csds.txt"
+data_path = base_path + "input/4.1_python.txt"
+# data_path = base_path + "input/a_few_courses.txt"
 
 batch_size = 128
 sequence_len = 40
 
-epochs = 60
-
-gen_text_freq = 20
-gen_text_length = 150
-
 ret = stateful_training_data_from_file(data_path, batch_size, sequence_len, validation_split=0.05)
-X_train, Y_train, X_validate, Y_validate, char_indices, indices_char, text, chars = ret
+_, _, _, _, char_indices, indices_char, text, chars = ret
 num_chars = len(char_indices)
 
+model_path = base_path + "model_checkpoints/stateful_3_512_100_1e-4_4.1_python/99-1.16-1.05.hdf5"
+# model_path = base_path + "model_checkpoints/stateful_128_a_few_courses/150-1.20-1.13.hdf5"
+# model_path = base_path + "model_checkpoints/stateful_2_512_100_few_courses_lr_decay/250-1.06-1.06.hdf5"
 
-model_path = base_path + "model_checkpoints/20220413_153403/40-1.06-1.31.hdf5"
 model = keras.models.load_model(model_path)
 model.summary()
 print()
 
 model_lstm1 = model.layers[0]
 model_lstm2 = model.layers[2]  # There is a dropout layer at index 1
+model_lstm3 = model.layers[4]  # There is a dropout layer at index 1
+
 units = model_lstm1.units
 
-inputs = Input(batch_input_shape=(1, model_lstm1.input_shape[1], model_lstm1.input_shape[2]))
+inputs = Input(batch_input_shape=(1, 1, model_lstm1.input_shape[2]))
 lstm1, state_h1, state_c1 = LSTM(model_lstm1.units, return_sequences=True, return_state=True, stateful=True)(inputs)
 lstm2, state_h2, state_c2 = LSTM(model_lstm1.units, return_sequences=True, return_state=True, stateful=True)(lstm1)
 
-model2 = Model(inputs=[inputs], outputs=[lstm1, state_h1, state_c1, lstm2, state_h2, state_c2])
+# 3rd layer
+lstm3, state_h3, state_c3 = LSTM(model_lstm1.units, return_sequences=True, return_state=True, stateful=True)(lstm2)
+
+# model2 = Model(inputs=[inputs], outputs=[lstm1, state_h1, state_c1, lstm2, state_h2, state_c2])
+model2 = Model(inputs=[inputs], outputs=[lstm1, state_h1, state_c1, lstm2, state_h2, state_c2, lstm3, state_h3, state_c3])
 
 # For faster computation, don't need to compute batch size of 128
 out = Dense(num_chars, activation="softmax")(lstm2)
@@ -47,37 +51,60 @@ model3 = Model(inputs=[inputs], outputs=[out])
 
 model2.layers[1].set_weights(model_lstm1.get_weights())
 model2.layers[2].set_weights(model_lstm2.get_weights())
+model2.layers[3].set_weights(model_lstm3.get_weights())
+
 model2.summary()
 print()
 
 
-model3.set_weights(model.get_weights())
+# model3.set_weights(model.get_weights())
 model3.summary()
 print()
 
-# # because including input layer, no dropout. 4 becuase including dropout, but no input layer
-n_hidden_vectors = 6
+# # because including input layer, no dropout. 4 because including dropout, but no input layer
+n_hidden_vectors = 9
+# n_hidden_vectors = 6
 
-diversity = 0.7
-length = 1000
-print("Diversity:", diversity)
 
+diversity = 0.85  # For good python results
+# diversity = 0.8 # For good courses results
+length = 100
+
+start = time.time()
+
+# The resulting text
 generated = ""
+
+# Sample a sentence to warmup the network with
 start_index = random.randint(0, len(text) - sequence_len - 1)
 sentence = text[start_index:start_index + sequence_len]
-print("Generating with: " + sentence)
 
 stored_hidden_states = np.empty((length, n_hidden_vectors * units))
 
-next_char = "t"
+print("Diversity:", diversity)
+print("Generating with: " + sentence)
+
+# Feed the network the sampled sentence as a warmup
+for i in range(len(sentence) - 1):
+    x_pred = np.zeros((1, 1, len(char_indices)))
+    x_pred[0, 0, char_indices[sentence[i]]] = 1.0
+
+    model.predict(x_pred)
+    model2.predict(x_pred)
+
+# Now, starting with the last char, feed the chars and sample a char as output
+next_char = sentence[-1]
+
 for i in range(length):
+    # One hot encoded vector for input char
     x_pred = np.zeros((1, 1, len(char_indices)))
     x_pred[0, 0, char_indices[next_char]] = 1.0
 
-    preds = model.predict(x_pred, verbose=0)[0, 0]
-
+    # Get predictions for our base model, and the model with the same weights that returns hidden states
+    preds = model.predict(x_pred)[0, 0]
     hidden_states = model2.predict(x_pred)
 
+    # Save hidden weights in our matrix
     for j in range(n_hidden_vectors):
         if len(hidden_states[j].shape) == 3:
             # return sequences is true so we need to get only the last value
@@ -85,12 +112,20 @@ for i in range(length):
         else:
             stored_hidden_states[i, j*units:(j+1)*units] = hidden_states[j][0]
 
+    # Sample a random char
     next_index = sample(preds, diversity)
     next_char = indices_char[next_index]
     generated += next_char
 
+model.reset_states()
+model2.reset_states()
+
+end = time.time()
+
+print("Took: {:.3f}s".format(end - start))
 print("Result: " + generated)
 
+# Save the text and hidden weights
 folder = "hidden_state_viz_data"
 hidden_data_file = folder + "/hidden_data.csv"
 text_file = folder + "/generated_text.txt"
@@ -98,3 +133,4 @@ text_file = folder + "/generated_text.txt"
 np.savetxt(hidden_data_file, stored_hidden_states, delimiter=",")
 with open(text_file, 'w') as f:
     f.write(generated)
+
