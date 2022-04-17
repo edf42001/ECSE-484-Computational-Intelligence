c57d0f5f6929283ffd18dcc1277b92b85a229c6d
Ethan Frank <edf32@case.edu>
2022-04-16 20:07:53 -0400

    Fix bug in how validation data was split for stateful

--- prepare_training_data_from_file.py
+++ prepare_training_data_from_file.py
@@ -2,29 +2,65 @@ import io
 import numpy as np
 
 
-def divide_text_into_sequences(text: str, num_batches: int, batch_size: int,
-                               sequence_len: int, char_indices) -> np.ndarray:
-    # Interleave the values into the array such that the batches are lined up
-    X = np.array([c for c in text])
-    X = X.reshape((batch_size, -1))
-    X2 = np.zeros((num_batches, batch_size, sequence_len), dtype='str')
+def char_array_to_one_hot(X, char_indices):
+    samples, sequence_len = X.shape
 
+    num_chars = len(char_indices)
+    # TODO: Investigate: is dtype=bool why my google notebook is running twice as fast? Or did they allocate me a better gpu?
+    one_hot = np.zeros((samples, sequence_len, num_chars), dtype=bool)
+
+    for i in range(samples):
+        for t in range(sequence_len):
+            one_hot[i, t, char_indices[X[i, t]]] = 1
+
+    return one_hot
+
+
+def get_val_split(X, sequence_length, val_split):
+    batch_size, total_length = X.shape
+
+    skim_amount = total_length * val_split
+    skim_amount = int(sequence_length * np.ceil(skim_amount / sequence_length))
+
+    # Returns: x_train, x_val. Skim a bit of each sequence off the end to use as validation
+    return X[:, :-skim_amount], X[:, -skim_amount:]
+
+
+def interleave(X, batch_size, sequence_len):
+    num_batches = X.shape[1] // sequence_len
+
+    # Interleave the values into the array such that the batches are lined up
+    X2 = np.zeros((num_batches, batch_size, sequence_len), dtype='str')
     for i, x in enumerate(X):
         something = np.expand_dims(x.reshape(-1, sequence_len), axis=1)
         X2[:, i::batch_size, :] = something
 
-    X2 = X2.reshape((-1, sequence_len))
-
-    num_chars = len(char_indices)
-    X3 = np.zeros((num_batches * batch_size, sequence_len, num_chars))
-    for i in range(num_batches * batch_size):
-        for t in range(sequence_len):
-            X3[i, t, char_indices[X2[i, t]]] = 1
-
-    return X3
+    return X2.reshape((-1, sequence_len))
 
 
-def stateful_training_data_from_file(filename: str, batch_size: int = 128, sequence_len: int = 50, validation_split: float = 0.05):
+def divide_text_into_sequences(text: str, num_batches: int, batch_size: int,
+                               sequence_len: int, val_split):
+    # Split the input array into batch_size long sequences
+    # These are the sequences that a copy of each stateful LSTM will train on in batches in parallel
+    X = np.array([c for c in text])
+    X = X.reshape((batch_size, -1))
+
+    # Now that we have these sequences, reserve some off the end of each for validation
+    # It's not random, but as long as there isn't an even divisible pattern in the text,
+    # (vanishingly likelky), then these sequences off the end represent a pretty random sample
+    X_train, X_val = get_val_split(X, sequence_len, val_split)
+
+    # Now, interleave these sequences such that when batches by keras, each index of each
+    # batch is the continuing sequence of the same index of the previous batch,
+    # # meaning wherever we split a sequence it is interleaved with batch_size
+    # other sequences. (for a stateful LSTM to work)
+    X2_train = interleave(X_train, batch_size, sequence_len)
+    X2_val = interleave(X_val, batch_size, sequence_len)
+
+    return X2_train, X2_val
+
+
+def stateful_training_data_from_file(filename: str, batch_size: int = 128, sequence_len: int = 50, val_split: float = 0.05):
     print("Reading data from " + filename)
     with io.open(filename, encoding="utf-8") as f:
         text = f.read().lower()
@@ -48,34 +84,22 @@ def stateful_training_data_from_file(filename: str, batch_size: int = 128, seque
     num_chars = len(chars)
     print("Number of different chars:", num_chars)
 
-    X = divide_text_into_sequences(text[:-1], num_batches, batch_size, sequence_len, char_indices)
-    Y = divide_text_into_sequences(text[1:], num_batches, batch_size, sequence_len, char_indices)
+    X_train, X_val = divide_text_into_sequences(text[:-1], num_batches, batch_size, sequence_len, val_split)
+    Y_train, Y_val = divide_text_into_sequences(text[1:], num_batches, batch_size, sequence_len, val_split)
 
-    num_sequences = X.shape[0]
-    print("Data shape:", X.shape)
+    X_train = char_array_to_one_hot(X_train, char_indices)
+    X_val = char_array_to_one_hot(X_val, char_indices)
+    Y_train = char_array_to_one_hot(Y_train, char_indices)
+    Y_val = char_array_to_one_hot(Y_val, char_indices)
 
-    # Pick some indices that will be for validation
-    n_validation = num_sequences * validation_split
-    n_validation = int(batch_size * np.ceil(n_validation / batch_size))  # Round up to nearest batch size
-    validation_indices = np.random.choice(num_sequences, size=n_validation, replace=False)
+    print("X/Y train shape:", X_train.shape)
+    print("X/Y val shape: ", X_val.shape)
 
-    # Extract these sequences for validation
-    X_validate = X[validation_indices]
-    Y_validate = Y[validation_indices]
-
-    # THe other portion is for training
-    X_train = np.delete(X, validation_indices, axis=0)
-    Y_train = np.delete(Y, validation_indices, axis=0)
-
-    print(X_train.shape)
-    print(Y_train.shape)
-    print(X_validate.shape)
-    print(Y_validate.shape)
-
-    return X_train, Y_train, X_validate, Y_validate, char_indices, indices_char, text, chars
+    return X_train, Y_train, X_val, Y_val, char_indices, indices_char, text, chars
 
 
 if __name__ == "__main__":
     filename = "input/alphabet.txt"
-    stateful_training_data_from_file(filename, batch_size=2, sequence_len=3, validation_split=0.3)
+
+    stateful_training_data_from_file(filename, batch_size=5, sequence_len=5, val_split=0.05)
 
